941a7a108fb1054462a6df82afe3fab65a08b424
sebdavid <121909326+sebs-commits@users.noreply.github.com>
2024-01-19 20:05:12 -0500

    created simple dice roller program

---
+++ dice_roller/script.js
@@ -0,0 +1,17 @@
+// dice roller program
+
+function rollDice() {
+  const numOfDice = document.getElementById("numOfDice").value;
+  const diceResult = document.getElementById("textResult");
+  const diceImages = document.getElementById("diceImages");
+  const values = [];
+  const images = [];
+
+  for (let i = 0; i < numOfDice; i++) {
+    const value = Math.floor(Math.random() * 6) + 1;
+    values.push(value);
+    images.push(`<img src="dice_images/dice${value}.png" alt="${value}">`);
+  }
+  diceResult.textContent = `Dice: ${values.join(", ")}`;
+  diceImages.innerHTML = images.join("");
+}
