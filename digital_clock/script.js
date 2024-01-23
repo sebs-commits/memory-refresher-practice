ee4aa8e368815a596be75dace1adb96329843ad7
sebdavid <121909326+sebs-commits@users.noreply.github.com>
2024-01-23 12:30:53 -0500

    added clock program

---
+++ digital_clock/script.js
@@ -0,0 +1,11 @@
+function updateClock() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  const timeString = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("clock").textContent = timeString;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
